9ecc27cc41939e1c715ae7aeb14ead6a34267d2d
evanstom273 <60609303+evanstom273@users.noreply.github.com>
2026-09-22 06:30:45 +0100

    feat: add shrine loadout slot capacities

--- scripts/resources/spell_loadout_definition.gd
+++ scripts/resources/spell_loadout_definition.gd
@@ -3,4 +3,6 @@ class_name SpellLoadoutDefinition
 extends Resource
 
 @export var display_name: String = "Spell loadout"
+@export_range(1, 12, 1) var base_slots: int = 3
+@export_range(1, 12, 1) var maximum_slots: int = 10
 @export var spells: Array[SpellDefinition] = []
